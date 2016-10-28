9e57f3665f2734e70ad04d9424ebb32fe6f95a0d
Kamila Souckova <kamila@ksp.sk>
2016-10-28 17:42:46 +0200

    BEST IDEA EVER!!!

---
+++ templates/bin/term_in_cwd.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Evil magic: parses the active window title, looking for something that looks
+# like a path, and opens a terminal in that directory.
+
+# from the first / to the end -- works for both terminal & file manager
+DIR="$(xdotool getactivewindow getwindowname | grep -o '/.*$')"
+[ -d "$DIR" ] || DIR="$HOME"
+{{REAL_TERM}} -d "$DIR"
