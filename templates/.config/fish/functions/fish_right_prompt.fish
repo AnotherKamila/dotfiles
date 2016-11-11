2367d3233908fad736274b3fbbc4dcbd9ce10c3a
Kamila Souckova <kamila@ksp.sk>
2016-11-11 12:33:04 +0100

    even more awesome git prompt

--- templates/.config/fish/functions/fish_right_prompt.fish
+++ templates/.config/fish/functions/fish_right_prompt.fish
@@ -3,13 +3,16 @@ function get_git_status -d "Gets the current git status"
     set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
     set -l ref (command git symbolic-ref --short HEAD 2> /dev/null ; or command git describe --tags --exact-match ^/dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
     set -l tag (command git describe --abbrev=0 2>/dev/null)
-
+    set -l ahead (command git status | grep ahead)
 
     if [ "$dirty" != "0" ]
       set_color red
       echo "*$dirty "
     else
       set_color cyan
+    end
+    if [ -n "$ahead" ]
+      echo "↑ "
     end
     echo "$ref"
     if [ -n "$tag" ]
