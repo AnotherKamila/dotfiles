269c7cadf2f95fbb293539828be111fe10460a74
Kamila Souckova <kamila@ksp.sk>
2016-11-11 12:07:57 +0100

    shell + git goodies

--- templates/.config/fish/aliases.fish
+++ templates/.config/fish/aliases.fish
@@ -1,6 +1,13 @@
 alias e  '{{EDITOR}}'
+alias l 'ls'
+alias t 'tree -F'
+alias o 'xdg-open'
 
 alias st 'git status'
 alias gp 'git add -p'
 
 alias py 'ptpython'
+
+alias u 'cd ~/u'
+
+alias dote "{{EDITOR}} ~/.dotfiles/params ~/.dotfiles/templates ~/.dotfiles/decrypted ~/.dotfiles/params/(hostname).yml"
--- templates/.config/fish/functions/fish_right_prompt.fish
+++ templates/.config/fish/functions/fish_right_prompt.fish
@@ -1,7 +1,8 @@
 function get_git_status -d "Gets the current git status"
   if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
     set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-    set -l ref (command git describe --tags --exact-match ^/dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+    set -l ref (command git symbolic-ref --short HEAD 2> /dev/null ; or command git describe --tags --exact-match ^/dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+    set -l tag (command git describe --tags --exact-match ^/dev/null)
 
     if [ "$dirty" != "0" ]
       set_color red
@@ -10,6 +11,9 @@ function get_git_status -d "Gets the current git status"
       set_color cyan
     end
     echo "$ref"
+    if [ -n "$tag" ]
+      echo " [$tag]"
+    end
    end
 end
 
