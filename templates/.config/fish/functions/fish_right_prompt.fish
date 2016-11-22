bf5b19ab8eece44072dc85aa3d8d375d3d98dae4
Kamila Souckova <kamila@ksp.sk>
2016-11-22 17:39:58 +0100

    fix git prompt

--- templates/.config/fish/functions/fish_right_prompt.fish
+++ templates/.config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,6 @@
 function get_git_status -d "Gets the current git status"
   if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l 2> /dev/null)
+    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
     set -l ref (command git symbolic-ref --short HEAD 2> /dev/null ; or command git describe --tags --exact-match ^/dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
     set -l tag (command git describe --abbrev=0 2>/dev/null)
     set -l ahead (command git status | grep ahead)
