116b6a0f9e5ef86283a1ad899fd90c59bdf1f85d
Kamila Souckova <kamila@ksp.sk>
2016-12-21 17:26:44 +0100

    add a few aliases

--- templates/.config/fish/aliases.fish
+++ templates/.config/fish/aliases.fish
@@ -10,9 +10,13 @@ alias computer, "sudo"  # best alias ever
 alias st 'git status'
 alias gp 'git add -p'
 
-alias py 'ptpython'
+alias py  'ptpython'
+alias py2 'ptpython2'
 
 alias u 'cd ~/u'
+alias shortps "alias fish_prompt \"echo '> '\""
 
 alias dote "{{EDITOR}} ~/.dotfiles/params ~/.dotfiles/templates ~/.dotfiles/decrypted ~/.dotfiles/params/(hostname).yml"
 alias dotpull "cd ~/.dotfiles; git pull ; redot.sh"
+
+alias serveme 'python3 -m http.server'
